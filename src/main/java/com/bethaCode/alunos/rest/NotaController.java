34c4e639b56f904d24bd1477c3f975ef19e2f4f6
tiagorv <tiago.valerio@betha.com.br>
2021-10-30 10:13:30 -0300

    Projeto Criado em sala de aula em 16/10

---
+++ src/main/java/com/bethaCode/alunos/rest/NotaController.java
@@ -0,0 +1,63 @@
+package com.bethaCode.alunos.rest;
+
+import com.bethaCode.alunos.model.dto.NotaDTO;
+import com.bethaCode.alunos.model.entity.Aluno;
+import com.bethaCode.alunos.model.entity.Disciplina;
+import com.bethaCode.alunos.model.entity.Nota;
+import com.bethaCode.alunos.model.repository.AlunoRepository;
+import com.bethaCode.alunos.model.repository.DisciplinaRepository;
+import com.bethaCode.alunos.model.repository.NotaRepository;
+import com.bethaCode.alunos.util.BigDecimalConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequestMapping("/api/notas")
+@RequiredArgsConstructor
+public class NotaController{
+
+    private final NotaRepository notaRepository;
+    private final AlunoRepository alunoRepository;
+    private final DisciplinaRepository disciplinaRepository;
+    private final BigDecimalConverter bigDecimalConverter;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Nota salvar(@RequestBody NotaDTO notaDTO){
+
+        LocalDate dataDaNota = LocalDate.parse(
+                                    notaDTO.getDataNota(),
+                                    DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+        BigDecimal valorDaNota = bigDecimalConverter.converter(notaDTO.getNota());
+
+        Integer idAluno = notaDTO.getIdAluno();
+        Aluno valorDoAluno = alunoRepository
+                            .findById(idAluno)
+                            .orElseThrow(()-> new ResponseStatusException(
+                                    HttpStatus.BAD_REQUEST,
+                                    "O aluno " + idAluno + " não existe em nossa app!"
+                            ));
+
+        Integer idDisciplina = notaDTO.getIdDisciplina();
+        Disciplina valorDaDisciplina = disciplinaRepository
+                                            .findById(idDisciplina)
+                                            .orElseThrow(()-> new ResponseStatusException(
+                                                    HttpStatus.BAD_REQUEST,
+                                                    "A Disciplina " + idDisciplina + " não existe em nosso app!"
+                                            ));
+
+        Nota notaASalvar = new Nota();
+        notaASalvar.setDataNota(dataDaNota);
+        notaASalvar.setNota(valorDaNota);
+        notaASalvar.setAluno(valorDoAluno);
+        notaASalvar.setDisciplina(valorDaDisciplina);
+        return notaRepository.save(notaASalvar);
+    }
+}
